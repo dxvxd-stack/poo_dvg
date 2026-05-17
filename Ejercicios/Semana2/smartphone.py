5808277e3b31e10a57e62fafa6293cfde9cc66cd
dxvxd-stack <1725110435@utectulancingo.edu.mx>
2026-05-17 18:57:46 +0000

    CREATED smartphone.py

---
+++ Ejercicios/Semana2/smartphone.py
@@ -0,0 +1,28 @@
+class Smartphone:
+    def __init__(self, marca, modelo, color, almacenamiento, ram, sistema_operativo, condicion_bateria, no_botones, resolucion_pantalla, num_camaras):
+        self.marca = marca
+        self.modelo = modelo
+        self.color = color
+        self.almacenamiento = almacenamiento
+        self.ram = ram
+        self.sistema_operativo = sistema_operativo
+        self.condicion_bateria = condicion_bateria
+        self.no_botones = no_botones
+        self.resolucion_pantalla = resolucion_pantalla
+        self.num_camaras = num_camaras
+
+    def mostrar_datos(self):
+        print(f"Marca del smartphone: {self.marca}")
+        print(f"Modelo del dispositivo: {self.modelo}")
+        print(f"Color del equipo: {self.color}")
+        print(f"Capacidad de almacenamiento: {self.almacenamiento} GB")
+        print(f"Memoria RAM: {self.ram} GB")
+        print(f"Sistema Operativo: {self.sistema_operativo}")
+        print(f"Condicion de batería: {self.condicion_bateria}%")
+        print(f"¿Cuantos botones tiene?: {self.no_botones}")
+        print(f"Resolución de pantalla: {self.resolucion_pantalla}")
+        print(f"Número de cámaras: {self.num_camaras}")
+
+# Ejemplo de instancia respetando tu formato
+iphone_13 = Smartphone("Apple", "iPhone 13", "Azul marino", 128, 4, "iOS", 84, 4, "2556 x 1179", 3)
+iphone_13.mostrar_datos()
