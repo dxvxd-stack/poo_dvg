fb0ac4b549f334c2db456b8a719a7b97389dfd4d
dxvxd-stack <1725110435@utectulancingo.edu.mx>
2026-05-19 13:11:40 +0000

    CREATED tramsporte.py

--- Ejercicios/Semana2/tramsporte.py
+++ Ejercicios/Semana2/tramsporte.py
@@ -1,57 +1,40 @@
-class Transporte:
-    def __init__(self, marca, modelo, anio, color, velocidad, pasajeros, peso, origen, km, en_movimiento):
+class Transporte: 
+    def __init__(self, marca, modelo, anio, color, capacidad_de_pasajeros, peso, pais_de_origen, kilometraje, 
+                 en_movimiento, velocidad_maxima):
         self.marca = marca
         self.modelo = modelo
         self.anio = anio
         self.color = color
-        self.velocidad = velocidad
-        self.pasajeros = pasajeros
+        self.capacidad_de_pasajeros = capacidad_de_pasajeros
+        self.velocidad_maxima = velocidad_maxima
         self.peso = peso
-        self.origen = origen
-        self.km = km
+        self.pais_de_origen = pais_de_origen
+        self.kilometraje = kilometraje
         self.en_movimiento = en_movimiento
-
     def arrancar(self):
         self.en_movimiento = True
-        print(f"El {self.marca} {self.modelo} ha arrancado.")
-
+        print(f"{self.marca} {self.modelo} ha arrancado.")
     def detener(self):
         self.en_movimiento = False
-        print(f"El {self.marca} {self.modelo} se ha detenido.")
-
-    def acelerar(self, cantidad):
-        print(f"Acelerando a {cantidad} Km/h.")
-
-    def frenar(self, cantidad):
-        print(f"Frenando {cantidad} Km/h.")
-
-    def obtener_info(self):
-        print(f"""
-        --- INFO DEL TRANSPORTE ---
-        Marca: {self.marca} | Modelo: {self.modelo} ({self.anio})
-        Color: {self.color} | País: {self.origen}
-        Pasajeros: {self.pasajeros} | Peso: {self.peso} Kg
-        Kilometraje: {self.km} Km | ¿Se mueve?: {self.en_movimiento}
-        """)
-
-    def cambiar_color(self, nuevo_color):
-        self.color = nuevo_color
-        print(f"Color cambiado a: {self.color}")
-
-    def tipo_transporte(self):
-        return "Transporte Terrestre"
-
-    def calcular_autonomia(self):
-        print("Autonomía no calculada todavía.")
-
-
-
-transporte1 = Transporte("Mercedes", "Sprinter", 2020, "Blanco", 120, 20, 3500, "Alemania", 15000, False)
-
+        print(f"{self.marca} {self.modelo} se ha detenido.")
+    def acelerar(self, velocidad):
+        print(f"{self.marca} {self.modelo} está acelerando a {velocidad} km/h.")
+    def frenar(self):
+        print(f"{self.marca} {self.modelo} está frenando.")
+    def mostrar_info(self):
+        print(f"Marca: {self.marca}")
+        print(f"Modelo: {self.modelo}")
+        print(f"Año: {self.anio}")
+        print(f"Color: {self.color}")
+        print(f"Capacidad de pasajeros: {self.capacidad_de_pasajeros}")
+        print(f"Peso: {self.peso} kg")
+        print(f"País de origen: {self.pais_de_origen}")
+        print(f"Kilometraje: {self.kilometraje} km")
+        print(f"En movimiento: {'Sí' if self.en_movimiento else 'No'}")
+        print(f"Velocidad máxima: {self.velocidad_maxima} km/h")
+transporte1 = Transporte("Mercedes Benz", "Marcopolo", 2010, "Rojo", 45, 2400, "Mexico(Nuevo leon)", 15000, False,"130 km/h" )
+transporte1.mostrar_info()
 transporte1.arrancar()
 transporte1.acelerar(60)
-transporte1.frenar(20)
-transporte1.cambiar_color("Gris")
-transporte1.obtener_info()
-transporte1.calcular_autonomia()
+transporte1.frenar()
 transporte1.detener()
